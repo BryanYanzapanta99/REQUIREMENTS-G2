dcce86925972d7c56dcb2eec646209c1d7b6ba36
Bryan Sebastián Yanzapanta Ontaneda <bsyanzapanta@espe.edu.ec>
2022-08-24 21:05:21 -0500

    app1

---
+++ app1/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './components/Home';
+import About from './components/About';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route  path="/" element={<Home />}/>
+        <Route  path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
